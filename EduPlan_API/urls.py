25e924ff261cdf40245f2af6017cb62d0d374b06
IsaacMrls <161041146+IsaacMrls@users.noreply.github.com>
2026-04-15 23:43:07 -0600

    Commit

--- EduPlan_API/urls.py
+++ EduPlan_API/urls.py
@@ -1,33 +1,31 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
-from EduPlan_API.views.auth import CustomAuthToken, Logout
-from EduPlan_API.views.bootstrap import VersionView
-from EduPlan_API.views.users import (
-	DocenteDetailView,
-	DocentesView,
-	EstudianteDetailView,
-	EstudiantesView,
-	Userme,
-	UsersView,
+from .views.auth import CustomAuthToken, Logout
+from .views.bootstrap import VersionView
+from .views.users import (
+    DocenteDetailView,
+    DocentesView,
+    EstudianteDetailView,
+    EstudiantesView,
+    Userme,
+    UsersView,
 )
 
-
 urlpatterns = [
-	path("admin/", admin.site.urls),
-	path("api-auth/", include("rest_framework.urls")),
-	path("api/version/", VersionView.as_view(), name="api-version"),
-	path("api/auth/login/", CustomAuthToken.as_view(), name="api-login"),
-	path("api/auth/logout/", Logout.as_view(), name="api-logout"),
-	path("api/users/", UsersView.as_view(), name="api-users"),
-	path("api/users/me/", Userme.as_view(), name="api-users-me"),
-	path("api/docentes/", DocentesView.as_view(), name="api-docentes"),
-	path("api/docentes/<int:pk>/", DocenteDetailView.as_view(), name="api-docente-detail"),
-	path("api/estudiantes/", EstudiantesView.as_view(), name="api-estudiantes"),
-	path("api/estudiantes/<int:pk>/", EstudianteDetailView.as_view(), name="api-estudiante-detail"),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/version/", VersionView.as_view(), name="api-version"),
+    path("api/auth/login/", CustomAuthToken.as_view(), name="api-login"),
+    path("api/auth/logout/", Logout.as_view(), name="api-logout"),
+    path("api/users/", UsersView.as_view(), name="api-users"),
+    path("api/users/me/", Userme.as_view(), name="api-users-me"),
+    path("api/docentes/", DocentesView.as_view(), name="api-docentes"),
+    path("api/docentes/<int:pk>/", DocenteDetailView.as_view(), name="api-docente-detail"),
+    path("api/estudiantes/", EstudiantesView.as_view(), name="api-estudiantes"),
+    path("api/estudiantes/<int:pk>/", EstudianteDetailView.as_view(), name="api-estudiante-detail"),
 ]
 
 if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
